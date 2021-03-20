fce9ca62710b82c1a9b7af7960689f28dbc3fc1d
2017-v <75252470+2017-v@users.noreply.github.com>
2021-03-20 10:26:48 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,47 @@
+// // alert("hello world");
+// var a=42;
+// var b=12;
+// // var c=a+b;
+// // console.log(c);
+// if(a>b)
+//   alert("you are idiots");
+// else
+//   alert( "you are again idiots" );
+//  var users=function(num1,num2){
+//     c=(num1+num2)
+//      alert("hello vikash");
+//      alert(c)
+//  }
+//  users(4,16);
+// var a=10;
+// var b=10;
+// var c=10;
+// var d=10;
+// a*=5
+// b+=2
+// c-=2
+// d/=5
+// alert(a);
+// alert(b);
+// alert(c);
+// alert(d);
+// var condition=function(a,b){
+// if(a>b)
+//   alert("you are a kid");
+// }
+// // else if(a<b){
+//     alert("you are not a kid");
+// }
+// else if(a==b){
+//     alert("you are in young age");
+// }
+// else{
+//     alert("you are old man");
+// }
+// }
+// condition(4,5);
+var a=5;
+while (a=5){
+alert("welcome vikash")
+}
+  a++
